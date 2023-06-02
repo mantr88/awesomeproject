9bd25594cd0de6333cf6ed481065f4d5fbf30318
AntonPetrenko <109439896+mantr88@users.noreply.github.com>
2023-06-03 01:49:50 +0300

    added markup in registration-screen component

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  ImageBackground,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import imageBG from "./src/image/Photo BG-2.jpeg";
+import { RegistrationScreen } from "./src/Screens/RegistrationScreen";
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground
+        source={imageBG}
+        resizeMode="cover"
+        style={styles.bgImage}
+      >
+        <RegistrationScreen />
+      </ImageBackground>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    color: "#212121",
+  },
+  bgImage: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
+
+export default App;
